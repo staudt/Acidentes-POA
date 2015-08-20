7915b38a05150a80c1a044ed98988a4be89c5ecd
staudt <ricardost@gmail.com>
2015-08-20 12:48:45 -0300

    dados compactados

---
+++ dados/compress_files.py
@@ -0,0 +1,11 @@
+import os
+import zlib
+
+
+if __name__ == '__main__':
+    for filename in os.listdir('.'):
+        if filename.endswith('.csv'):
+            with open(filename, 'rb') as f_read:
+                with open('%s.zlib' % filename, 'wb') as f_write:
+                    print 'Compressing file "%s" to "%s.zlib"' % (filename, filename)
+                    f_write.write(zlib.compress(f_read.read()))
